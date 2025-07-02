8f3c298df42cc3a4c9436ffb3122664fbad9461e
Adelie Gorce <adelie.gorce@gmail.com>
2025-07-02 17:43:18 +0200

    Improved cl21 method

--- cross-correlations/With_Pee/theory.py
+++ cross-correlations/With_Pee/theory.py
@@ -967,7 +967,7 @@ class Pee_model:
         P21 = np.where(P21<=0, 1.0e-36, P21)
         return np.log10(P21) if log else P21
 
-    def get_cl21(self, z21, ells, Dells=True, delta_nu=0.):
+    def get_cl21(self, z21, ells, Dells=True, delta_nu=0., nz=100):
         """
         Compute angular cross spectrum of 21cm at ell centred on z, in uK.
 
@@ -993,51 +993,61 @@ class Pee_model:
             H0=self.h * 100, Tcmb0=self.T_cmb, Ob0=self.Ob_0, Om0=self.Om_0
         )
 
+        delta_nu = delta_nu.to(units.MHz)
+
+        nu21_ref_unit = nu21_ref * units.MHz
+
+        if delta_nu == 0.:
+            zlin = np.linspace(z21-0.1, z21+0.2, nz)        
+        else:
+            nu21 = nu21_ref_unit / (1. + z21)  # MHz
+            numax = nu21 + delta_nu/2.
+            numin = nu21 - delta_nu/2.
+            zmax = (nu21_ref_unit / numin) - 1.
+            zmin = (nu21_ref_unit / numax) - 1.
+            zlin = np.linspace(zmin.value-0.1, zmax.value+0.2, nz)
         ells = np.atleast_1d(ells)
-        kmax_ells = np.max(ells) / cos.comoving_distance(z_integ.min()).value
-        delta_nu *= units.MHz
+        kmax_ells = np.max(ells) / cos.comoving_distance(zlin.min()).value
 
         if not self.has_camb_run:
             warnings.warn('Re-running CAMB...')
             self.run_camb(kmax_pk=min(kmax_ells, kmax_camb))
 
+        W21_zlin = np.zeros_like(zlin) * 1. / units.MHz
+        iz21 = np.argmin(np.abs(zlin - z21))
+
+        if delta_nu == 0.:
+            W21_zlin[iz21] = 1. / units.MHz
+        else:
+            nu_integ = nu21_ref_unit / (1. + zlin)  # MHz
+            # print(f'{delta_nu:.2f}, {np.abs(np.diff(nu_integ)[iz21]):.2f}')
+            if np.abs(np.diff(nu_integ)[iz21]) > delta_nu:
+                raise ValueError('dnu smaller than zlin resolution '
+                                 f'({delta_nu:.2f}, {np.abs(np.diff(nu_integ)[iz21]):.2f}), '
+                                 'increase nz.')
+            mask = (zlin < zmax) & (zlin >= zmin)
+            W21_zlin[mask] = 1./(numax-numin)  # unit T
+        W21_zlin *= nu21_ref_unit * cos.H(zlin).si / (1+zlin)**2 / constants.c.si  # units L-1
+        W21_zlin = W21_zlin.to(1./units.Mpc)
+        W21_zlin /= np.trapz(W21_zlin, cos.comoving_distance(zlin))
+
         # Define integration ranges
-        k_z_integ = ells[:, None] / cos.comoving_distance(z_integ).value  # [Mpc-1]
-        if (k_z_integ.min() < kmin_camb) or (k_z_integ.max() > kmax_camb):
+        k_zlin = ells[:, None] / cos.comoving_distance(zlin).value  # [Mpc-1]
+        if (k_zlin.min() < kmin_camb) or (k_zlin.max() > kmax_camb):
             raise ValueError("Extrapolating the matter PK too far.")
 
-        P21_z_integ = self.get_p21(k_z_integ, z_integ, mK=False, log=False, pk_units=True)
-        self.check_ps(P21_z_integ)
-
-        W21_z_integ = np.zeros_like(z_integ) * 1. / units.MHz
-        nu21_ref_unit = nu21_ref * units.MHz
-        iz21 = np.argmin(np.abs(z_integ - z21))
-        if delta_nu == 0.:
-            W21_z_integ[iz21] = 1. / units.MHz
-        else:
-            nu21 = nu21_ref_unit / (1. + z21) # MHz
-            nu_integ = nu21_ref_unit / (1. + z_integ) # MHz
-            if np.abs(np.diff(nu_integ)[iz21]) > delta_nu:
-                warnings.warn(f'dnu smaller than z_integ resolution ({delta_nu:.2f}, {np.abs(np.diff(nu_integ)[iz21]):.2f}).')
-            numax = nu21 + delta_nu/2.
-            numin = nu21 - delta_nu/2.
-            zmax = (nu21_ref_unit / numin) - 1.
-            zmin = (nu21_ref_unit / numax) - 1.
-            mask = (z_integ < zmax) & (z_integ >= zmin)
-            W21_z_integ[mask] = 1./(numax-numin)  # unit T
-        W21_z_integ *= nu21_ref_unit * cos.H(z_integ).si / (1+z_integ)**2 / constants.c.si  # units L-1
-        W21_z_integ = W21_z_integ.to(1./units.Mpc)
-        W21_z_integ /= np.trapz(W21_z_integ, cos.comoving_distance(z_integ))
+        P21_zlin = self.get_p21(k_zlin, zlin, mK=False, log=False, pk_units=True)
+        self.check_ps(P21_zlin)
 
         # Compute C_tau(ell) integrand, unit 1
         C_ell_21_integrand = (
             self.T0(z21) ** 2
-            / cos.comoving_distance(z_integ)**2
-            * P21_z_integ * (units.Mpc)**3
-            * W21_z_integ.to(1./units.Mpc)**2
+            / cos.comoving_distance(zlin)**2
+            * P21_zlin * (units.Mpc)**3
+            * W21_zlin.to(1./units.Mpc)**2
         )
         # Compute C_kSZ(ell), no units
-        C_ells = np.trapz(C_ell_21_integrand, cos.comoving_distance(z_integ), axis=1)
+        C_ells = np.trapz(C_ell_21_integrand, cos.comoving_distance(zlin), axis=1)
         self.check_result(C_ells)
 
         if not Dells:
@@ -1343,9 +1353,6 @@ class Pee_model:
 
         delta_nu *= units.MHz
 
-        if not self.has_camb_run:
-            warnings.warn('Re-running CAMB...')
-            self.run_camb(kmax_pk=min(kmax_ells, kmax_camb))
         nu21_ref_unit = nu21_ref * units.MHz
 
         if delta_nu == 0.:
@@ -1359,6 +1366,10 @@ class Pee_model:
             zlin = np.linspace(zmin.value-0.1, zmax.value+0.2, nz)
         ells = np.atleast_1d(ells)
         kmax_ells = np.max(ells) / cos.comoving_distance(zlin.min()).value
+
+        if not self.has_camb_run:
+            warnings.warn('Re-running CAMB...')
+            self.run_camb(kmax_pk=min(kmax_ells, kmax_camb))
 
         W21_zlin = np.zeros_like(zlin) * 1. / units.MHz
         iz21 = np.argmin(np.abs(zlin - z21))
