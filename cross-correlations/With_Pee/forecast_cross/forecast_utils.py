a435cc79ec9ab66ec5fe97e74d67341ca2244c05
Adelie Gorce <adelie.gorce@gmail.com>
2025-11-21 12:05:34 +0100

    Added forecast for taux21

---
+++ cross-correlations/With_Pee/forecast_cross/forecast_utils.py
@@ -0,0 +1,210 @@
+import sys, os, glob
+sys.path.insert(0, '../')
+
+import numpy as np
+from astropy import cosmology, units, constants
+from scipy.interpolate import RegularGridInterpolator, interp1d
+
+from theory import Pee_model
+from utils_tau import tau_noise
+
+from parameters import telescope_specs, nu21_ref
+
+theta_labels = [r'$z_\mathrm{re}$', r'd$z$', r'log$\alpha_0$', r'$\kappa$']
+prior_bounds = [(5.5, 10.), (0., 4.), (0, 6.), (0, 0.4)]
+
+
+def get_sensitivity(label, zs=[5.5, 6., 6.5, 7., 8., 9., 10., 11., 12., 14.], error_type='thermal', debug=False):
+
+    zs = np.sort(zs)[::-1]
+    freq_bands = np.round(nu21_ref / (zs + 1.0), decimals=1)  # MHz
+
+    sensitivity_at_z_arr = []
+    sensitivity_at_z_kbins = []
+    for iz in range(zs.size):
+        if error_type == 'both':
+            out1 = np.loadtxt(f"../sensitivity_21cm/{label}_thermal_{freq_bands[iz]}.txt")
+            out2 = np.loadtxt(f"../sensitivity_21cm/{label}_sample_{freq_bands[iz]}.txt")
+            out = out1 + out2
+        else:
+            out = np.loadtxt(f"../sensitivity_21cm/{label}_{error_type}_{freq_bands[iz]}.txt")
+        sensitivity_at_z_arr.append(out[:, 1])
+        sensitivity_at_z_kbins.append(out[:, 0])
+
+    sensitivity_at_z = RegularGridInterpolator(
+        (zs, sensitivity_at_z_kbins[0]),
+        np.array(sensitivity_at_z_arr),
+        method='linear',
+        fill_value=np.inf,
+        bounds_error=False
+    )
+
+    if debug:
+        print(np.isnan(sensitivity_at_z_arr).sum())
+        print(np.min(sensitivity_at_z_kbins), np.max(sensitivity_at_z_kbins))
+        print(sensitivity_at_z([6.8, 1.1]))
+
+    return sensitivity_at_z
+
+
+def get_cl21_noise(noise_function, z21, ells, Dells=True, delta_nu=0., nz=100, freq_res=100.*units.kHz, cos=cosmology.Planck18):
+    """
+    Compute angular cross spectrum of 21cm at ell centred on z, in uK.
+
+    Parameters
+    ----------
+        z: float
+            Redshift of the observed 21cm signal.
+        ells: array of floats
+            Angular multipole to compute the spectrum at.
+        Dells: boolean
+            If True, give the results in terms of
+            D(l) = l(l+1)Cl/2/pi.
+            Default is False.
+        delta_nu: float
+            Width of the top-hat function representing the
+            frequency resolution of the interferometer, in MHz.
+            Default is zero (Dirac delta).
+    Outputs
+    ------
+        Array of angular auto-power for ells, in uK2.
+    """
+
+    ells = np.atleast_1d(ells)
+    delta_nu *= units.MHz
+    nu21_ref_unit = nu21_ref * units.MHz
+
+    if delta_nu == 0.:
+        zlin = np.r_[np.linspace(z21-0.1, z21, nz//2), np.linspace(z21, z21+0.1, nz//2)[1:]]
+    else:
+        nu21 = nu21_ref_unit / (1. + z21)  # MHz
+        numax = nu21 + delta_nu/2.
+        numin = nu21 - delta_nu/2.
+        zmax = (nu21_ref_unit / numin) - 1.
+        zmin = (nu21_ref_unit / numax) - 1.
+        zlin = np.linspace(zmin.value-0.1, zmax.value+0.2, nz)
+
+    W21_zlin = np.zeros_like(zlin) * 1. / units.MHz
+    iz21 = np.argmin(np.abs(zlin - z21))
+    if delta_nu == 0.:
+        W21_zlin[iz21] = 1. / units.MHz
+    else:
+        nu_integ = nu21_ref_unit / (1. + zlin)  # MHz
+        # print(f'{delta_nu:.2f}, {np.abs(np.diff(nu_integ)[iz21]):.2f}')
+        if np.abs(np.diff(nu_integ)[iz21]) > delta_nu:
+            raise ValueError('dnu smaller than zlin resolution '
+                             f'({delta_nu:.2f}, {np.abs(np.diff(nu_integ)[iz21]):.2f}), '
+                             'increase nz.')
+        mask = (zlin < zmax) & (zlin >= zmin)
+        W21_zlin[mask] = 1./(numax-numin)  # unit T
+    W21_zlin *= nu21_ref_unit * cos.H(zlin).si / (1+zlin)**2 / constants.c.si  # units L-1
+    W21_zlin = W21_zlin.to(1./units.Mpc)
+    W21_zlin /= np.trapz(W21_zlin, cos.comoving_distance(zlin))
+
+    eta_lin = cos.comoving_distance(zlin).value
+    # Compute C_tau(ell) integrand, unit 1
+    C_ell_21_integrand = np.zeros((ells.size, zlin.size))
+    for j, ell in enumerate(ells):
+        k_zlin = ell / eta_lin  # [Mpc-1]
+        N21 = (noise_function(np.c_[zlin, k_zlin])/k_zlin**3*2*np.pi**2)
+        N21[np.isnan(N21) | np.isinf(N21)] = 0.
+        C_ell_21_integrand[j] = (
+            N21
+            / eta_lin**2
+            * W21_zlin.to(1./units.Mpc).value**2
+        )
+    # Compute C_kSZ(ell), no units
+    C_ells = np.trapz(C_ell_21_integrand, eta_lin, axis=1)
+    C_ells[C_ells == 0.] = np.inf
+    # account for average over frequency channels
+    C_ells /= np.sqrt(delta_nu.to(units.MHz).value / freq_res.to(units.MHz).value)
+
+    if not Dells:
+        return C_ells * units.mK**2
+    else:
+        return ells * (ells + 1.) * C_ells / 2. / np.pi * units.mK**2
+
+
+def make_datapoints(
+        theta, tel_tau, label21='ska_aast_optimistic',
+        delta_nu=100.*units.MHz, zs=[5.5, 6., 6.5, 7., 7.5, 8., 9., 10., 11., 12., 14.],
+        use_ksz_emulator=False, randomness=False,
+        cos=cosmology.Planck18, save=None, verbose=True):
+
+    if verbose:
+        print(f'\nComputing sensitivity for {tel_tau} x {label21}, '
+              f'with a {delta_nu:.1f} bandwidth.')
+
+    preion_model = Pee_model(
+        zre_h=theta[0], dz_h=theta[1],
+        alpha0=theta[2], kappa=theta[3],
+        h=cos.h, Ob_0=cos.Ob0, Om_0=cos.Om0,
+        verbose=False, run_camb=True,
+        use_ksz_emulator=use_ksz_emulator)
+
+    # tau
+    assert tel_tau in telescope_specs.keys()
+    lmin, lmax = telescope_specs[tel_tau]['lmin'], telescope_specs[tel_tau]['lmax']
+    dell = telescope_specs[tel_tau]['Delta_ell']
+    if tel_tau.find('SAT') >= 0:
+        ells = np.r_[lmin, np.arange(dell, lmax+dell, step=dell)]
+    else:
+        ells = np.arange(lmin, lmax+dell, step=dell)
+        if ells[-1] > lmax:
+            ells[-1] = lmax
+
+    datafile = f'../data/dltau_{tel_tau}.txt'
+    if os.path.exists(datafile):
+        ells2, Dl_tautau, Nl_tautau = np.loadtxt(datafile, unpack=True)
+        if not np.all(ells2 == ells):
+            print(f'Warning: {datafile} does not match the current ells, extrapolating...')
+            Dl_tautau = interp1d(ells2, Dl_tautau, bounds_error=False, fill_value='extrapolate')(ells)
+        print(f'Loaded data from {datafile}')
+    else:
+        print(f'No data file found at {datafile}, computing...')
+        Dl_tautau = np.sum(preion_model.get_tau(ells=ells, signal='both', Dells=True), axis=1)
+        ls_temp = np.arange(int(telescope_specs[tel_tau]['lmax'])+1)
+        CMB_Cells = preion_model.get_primary_spectra(ells=ls_temp, Dells=False)
+        CMB_Cells = CMB_Cells[:, [0, 1, 2, 4, 3]]
+        tau_temp = np.r_[0., np.sum(preion_model.get_tau(ells=ls_temp[1:], signal='both', Dells=False), axis=1)]
+        tau_noise_res = tau_noise(tel_tau, np.c_[CMB_Cells, tau_temp], is_cl=False)
+        Nl_tautau = interp1d(ls_temp, tau_noise_res)(ells)
+        np.savetxt(datafile, np.c_[ells, Dl_tautau, Nl_tautau], fmt='%.6e', header='l, Dl_tautau, Nl_tautau (noise only)')
+        print(f'Saved data to {datafile}.')
+    if randomness:
+        Dl_tautau = np.random.normal(Dl_tautau, Nl_tautau)
+
+    # 21cm obs parameters
+
+    if label21.find('ska') >= 0:
+        fov_ska = (9.10*units.deg)**2
+        fsky = fov_ska.to(units.rad**2)/(4.*np.pi*units.rad**2)
+        if label21.find('fields') >= 0:
+            fsky *= float(label21[label21.find('fields')-1])
+    elif label21.find('hera') >= 0:
+        fsky = 0.5*(np.cos(55*np.pi/180.)-np.cos(65*np.pi/180.))  # hera
+    elif label21.find('mwa') >= 0:
+        fov_mwa = (24.6*units.deg)**2
+        fsky = fov_mwa.to(units.rad**2)/(4.*np.pi*units.rad**2)  # mwa
+    sensitivity = get_sensitivity(label21, error_type='thermal')
+
+    # cross
+    Dl_tau21 = np.zeros((np.size(zs), np.size(ells)))
+    Dltau21_err = np.zeros((np.size(zs), np.size(ells)))
+    for iz, z21 in enumerate(zs):
+        Dl_21 = preion_model.get_cl21(z21, ells, Dells=True, delta_nu=delta_nu).to(units.uK**2).value
+        Nl_21 = get_cl21_noise(sensitivity, z21, ells, Dells=True, delta_nu=delta_nu.value).to(units.uK**2).value
+        if np.isinf(Nl_21).any():
+            print(f'Warning: 21cm noise values at l={ells[np.isinf(Nl_21)]} are infinite.')
+        Dl_tau21[iz] = preion_model.get_tau_21_cross(z21, ells, Dells=True, delta_nu=delta_nu.value).to(units.uK).value
+        Dltau21_err[iz] = 1. / (2.*ells+1) / fsky / telescope_specs[tel_tau]['Delta_ell'] * (
+                Dl_tau21[iz]**2 + ((Dl_21 + Nl_21) * (Dl_tautau + Nl_tautau))
+            )
+        if verbose:
+            print(f'z = {z21:.1f}, cumulative SNR = {np.sqrt(Dl_tau21[iz]**2 / Dltau21_err[iz]).sum():.2f}')
+
+    if (save is not None) and (len(glob.glob(f'data/taux21_{tel_tau}_{label21}_z*_datapoints.txt'))==0):
+        for iz, z21 in enumerate(zs):
+            np.savetxt(f'data/taux21_{tel_tau}_{label21}_z{z21:.1f}_datapoints.txt', np.c_[ells, Dl_tau21[iz], Dltau21_err[iz]], header='ell, Dl [uK], err [uK]')
+
+    return ells, Dl_tau21, Dltau21_err
---
+++ cross-correlations/With_Pee/forecast_cross/run_mcmc.py
@@ -0,0 +1,177 @@
+import sys
+sys.path.insert(0, '../')
+
+from matplotlib import rc
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy import cosmology, units
+import triangleme2 as triangleme
+import time
+import emcee
+import multiprocessing as mp
+import os
+
+from forecast_utils import make_datapoints
+from parameters import telescope_specs
+from theory import Pee_model
+
+cos = cosmology.Planck18
+
+randomness = False
+overwrite = True
+use_ksz_emulator = 'RF'
+
+label21 = 'hera_moderate'
+delta_nu = 100.*units.MHz
+zs = [7.]  # [5.5, 6., 6.5, 7., 8., 9., 10., 11., 12., 14.]
+
+tel_tau = 'CMB-S4-LAT'
+label = f'taux21_{tel_tau}_{label21}'
+
+
+niterations = 20#000
+nwalkers = 2#8
+ncores = mp.cpu_count()
+print(ncores)
+
+theta_true = [7.0, 1.5, 3.7, 0.10]
+ndim = len(theta_true)
+theta_labels = [r'$z_\mathrm{re}$', r'd$z$', r'log$\alpha_0$', r'$\kappa$']
+priors = [(5., 10.), (0.1, 4.5), (2.5, 4.5), (0.05, 0.4)]
+
+plot = True
+
+# mock data
+ells, tau21_data, tau21_err = make_datapoints(
+    theta_true, tel_tau,
+    delta_nu=delta_nu, label21=label21,
+    zs=zs,
+    randomness=randomness,
+    cos=cos, save=label, use_ksz_emulator=use_ksz_emulator,
+)
+
+preion_model = Pee_model(
+    h=cos.h, Ob_0=cos.Ob0, Om_0=cos.Om0,
+    verbose=False, run_camb=True,
+    use_ksz_emulator=use_ksz_emulator)
+preion_model.run_camb = False
+
+
+def get_model(theta, ells=ells):
+
+    preion_model.zre_h = theta[0]
+    preion_model.dz_h = theta[1]
+    preion_model.alpha0 = theta[2]
+    preion_model.kappa = theta[3]
+    preion_model.init_reionisation_history()
+
+    model = []
+    tau_ps, ps21 = [], []
+    for z21 in zs:
+        model.append(preion_model.get_tau_21_cross(z21, ells, Dells=True, delta_nu=delta_nu.value).to(units.uK).value)
+        ps21.append(preion_model.get_cl21(z21, ells, Dells=True, delta_nu=delta_nu).to(units.uK**2).value)
+        tau_ps.append(np.sum(preion_model.get_tau(ells=ells, signal='both', Dells=True), axis=1))
+
+    return np.array(model), np.array(tau_ps), np.array(ps21)
+
+
+def lnprob(theta):
+    lp = lnprior(theta)
+    if not np.isfinite(lp):
+        return -np.inf, 0., 0., 0.
+    ln, model, tau_ps, ps21 = lnlike(theta)
+    return lp + ln, model, tau_ps, ps21
+
+
+def lnlike(theta):
+    model, tau_ps, ps21 = get_model(theta)
+    m_inf = ~np.isinf(tau21_err)
+    like = (model[m_inf] - tau21_data[m_inf])**2 / tau21_err[m_inf]
+    return -0.5 * like.sum(), model, tau_ps, ps21
+
+
+def lnprior(theta, priors=priors):
+    for i, p in enumerate(priors):
+        low, high = p
+        if not (low <= theta[i] <= high):
+            return -np.inf
+    return 0.
+
+
+t0 = time.time()
+print(lnlike(theta_true)[0])
+t1 = time.time()
+print(f'It takes {t1-t0:.1f} seconds to compute one model.')
+# blobs & backend
+if os.path.isfile(f'backends/mcmc_{label}_backend.h5') and overwrite:
+    os.remove(f'backends/mcmc_{label}_backend.h5')
+backend = emcee.backends.HDFBackend(f'backends/mcmc_{label}_backend.h5')
+if overwrite:
+    backend.reset(nwalkers, ndim)
+dtype = [("Dl_tau21", float, (np.size(zs), np.size(ells),)),
+         ("Dl_tautau", float, (np.size(ells),)),
+         ("Dl_21", float, (np.size(zs), np.size(ells),)),]
+
+p0 = [np.random.uniform(low, high, size=nwalkers) for low, high in priors]
+p0 = np.vstack(p0).T
+print(p0)
+print(priors)
+
+with mp.Pool(ncores) as pool:
+    sampler = emcee.EnsembleSampler(
+        nwalkers, ndim, lnprob,
+        backend=backend,
+        pool=pool,
+        blobs_dtype=dtype,)
+    t0 = time.time()
+    state = sampler.run_mcmc(p0, niterations, progress=True)
+    t1 = time.time()
+print(f'It took {(t1-t0)/60./60.:.1f} hours to run {niterations} iterations with {nwalkers} walkers.')
+
+samples = sampler.get_chain(flat=False)
+
+# auto-correlation analysis to assess convergence and define burn-in
+taus = sampler.get_autocorr_time(tol=0)
+if np.isnan(taus).any():
+    print('NaN tau. Taking max.')
+endtau = np.nanmax(taus)
+converged = np.all(taus * 60 < sampler.iteration)
+print('Auto-correlation time: %.2f. Converged: %s.' %(endtau, converged))
+burnin = int(max(0.1*samples.shape[0],2.*np.max(taus)))
+print('burnin = %.1f' %(burnin/samples.shape[0]))
+
+flatsamples = sampler.get_chain(flat=True, discard=burnin)
+logps = sampler.get_log_prob(flat=True, discard=burnin)
+cov = np.cov(flatsamples.T)
+fom = np.sqrt(np.linalg.det(cov))
+print(f'FoM = {fom:.1e}')
+
+if plot:
+
+    fig = triangleme.corner(
+        flatsamples,
+        truths=theta_true,
+        labels=theta_labels, truth_color='k',
+        sigmas=[1, 2], plot_datapoints=False, 
+        # cmap='Reds', color='red', 
+        lw=2., smooth=1.,
+
+    )
+    axes = np.asarray(fig.axes)  # Access all the axes in the corner plot
+    axes = axes.reshape(np.size(theta_true), np.size(theta_true))
+    for i in range(1, ndim):       # Loop over rows (starting at 1 to skip diagonal)
+        for j in range(i):         # Loop over columns (lower triangle only)
+            axes[i, j].scatter(p0[:, j], p0[:, i], color='red', s=10, alpha=0.5)
+    fig.tight_layout()
+    fig.savefig(f'figures/mcmc_{label}_corner.png', dpi=220)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(
+        cov, cmap='coolwarm',
+        # norm=colors.LogNorm(),
+        extent=(0, 2, 2, 0))
+    ax.set_xticks(np.arange(0.25, 2, step=.5), labels=theta_labels)
+    ax.set_yticks(np.arange(0.25, 2, step=.5), labels=theta_labels)
+    cbar = plt.colorbar(im, fraction=0.045, ax=ax, label=r'$\mathcal{C}_{ij}$')
+    fig.tight_layout()
+    fig.savefig(f'figures/mcmc_{label}_covmat.png', dpi=220)
